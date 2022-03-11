32dc96ae980a122c5a80e16ab8bee6f050db19b0
Theophile <theophiledumashie@gmail.com>
2022-03-11 14:40:18 +0000

    Autre version d la methode opposer

--- src/pratiqueguit/Complexe.java
+++ src/pratiqueguit/Complexe.java
@@ -9,5 +9,41 @@ package pratiqueguit;
  * @author Theophile
  */
 public class Complexe {
+    private double reel ;
+    private double imaginaire ;
+
+    public Complexe(double reel, double imaginaire) {
+        this.reel = reel;
+        this.imaginaire = imaginaire;
+    }
+
+    public Complexe() {
+    }
+
+    public double getReel() {
+        return reel;
+    }
+
+    public void setReel(double reel) {
+        this.reel = reel;
+    }
+
+    public double getImaginaire() {
+        return imaginaire;
+    }
+
+    public void setImaginaire(double imaginaire) {
+        this.imaginaire = imaginaire;
+    }
+
+    @Override
+    public String toString() {
+        return "Complexe{" + "reel=" + reel + ", imaginaire=" + imaginaire + '}';
+    }
     
+    public Complexe opposer ( Complexe z)
+    {
+        return new Complexe(-z.reel, -z.imaginaire) ;
+    }
+            
 }
